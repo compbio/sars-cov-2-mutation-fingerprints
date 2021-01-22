4a6958900f5ea0f27a425f08d7c65177a63e9d5d
Dmitri Pavlichin <dmitripa@gmail.com>
2021-01-22 03:05:37 -0800

    add setup script and executable

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# download Julia 1.5.3
+JULIA_VERSION=1.5.3
+echo "## downloading Julia $JULIA_VERSION..."
+#curl -OJ "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz"
+
+echo "## unpacking Julia..."
+#tar -xzf julia-1.5.3-linux-x86_64.tar.gz
+
+echo "## setting up scripts..."
+export JULIA_DEPOT_PATH=$PWD/julia-1.5.3/depot
+./julia-1.5.3/bin/julia -e '@show DEPOT_PATH; import Pkg; Pkg.add(path="."); using sarscov2primers;'
+
+echo "## cleaning up..."
+rm julia-1.5.3-linux-x86_64.tar.gz
