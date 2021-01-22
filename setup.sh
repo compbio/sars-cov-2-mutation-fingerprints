21c9d607ca370ba53c2b8da75409bf39ef106bb0
Dmitri Pavlichin <dmitripa@gmail.com>
2021-01-22 04:47:11 -0800

    uncommented

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # download Julia 1.5.3
 JULIA_VERSION=1.5.3
 echo "## downloading Julia $JULIA_VERSION..."
-#curl -OJ "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz"
+curl -OJ "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz"
 
 echo "## unpacking Julia..."
-#tar -xzf julia-1.5.3-linux-x86_64.tar.gz
+tar -xzf julia-1.5.3-linux-x86_64.tar.gz
 
 echo "## setting up scripts..."
 export JULIA_DEPOT_PATH=$PWD/julia-1.5.3/depot
